36174a6996afb9df70091766cbebf3749821db4d
Patryk <vnlkbn@gmail.com>
2020-10-11 22:19:41 +0200

    9. Palindrome Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int test = 321;
-        System.out.println(ReverseInteger.reverse(test));
+        int test = -121;
+        System.out.println(PalindromeNumber.isPalindrome(test));
     }
 
 
---
+++ src/com/company/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        int rev = 0;
+        int y = x;
+
+
+        while (x != 0){
+            int pop = x % 10;
+            x /= 10;
+            rev = rev * 10 + pop;
+        }
+
+        if (y<0) rev *= -1;
+        if (rev == y) return  true;
+        else return false;
+
+
+    }
+}
