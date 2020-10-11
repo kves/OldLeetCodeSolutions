8be91042dbe90ea7d05a162690b0bace84ab99c0
Patryk <vnlkbn@gmail.com>
2020-10-11 19:42:31 +0200

    7. Reverse Integer

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int test = 321;
+        System.out.println(ReverseInteger.reverse(test));
+    }
+
+
+
+
+
+}
